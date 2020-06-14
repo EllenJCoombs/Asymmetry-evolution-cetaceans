ff278328f39e3cd56c5786437daae1d4129438bf
EllenJCoombs <ellen.coombs.14@ucl.ac.uk>
2020-06-14 15:22:23 +0100

    Update Phylo-ANOVAs_Benjamini-Hochberg.R

--- Code for analyses/Phylo-ANOVAs_Benjamini-Hochberg.R
+++ Code for analyses/Phylo-ANOVAs_Benjamini-Hochberg.R
@@ -1,9 +1,4 @@
 
-
-
-library(ape)
-library(nlme)
-
 #Phylo ANOVAs 
 #We ran phylogenetically corrected ANOVAs on each of the different scenarios using the R package nlme (v.3.1-137)
 #and function ‘gls’ to test for correlations between the level of asymmetry seen in the skull and the potential scenarios (or regimes)
@@ -11,6 +6,8 @@ library(nlme)
 #Simulations were run using a ‘Pagel’s Lambda’ (λ) correlation structure (corPagel) in the ape package which is derived from a 
 #Brownian Motion model by multiplying the covariances by λ. 
 
+library(ape)
+library(nlme)
 
 #Do this for all of the different 'regimes' 
 fit<-gls(sum.radii ~ as.factor(age), correlation=corBrownian(phy=tree), data = data) # or better use corPagel
